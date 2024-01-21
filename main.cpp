4214bca50069fd0ab8910a901ae5a4db0a4068dc
smorf1koo <gepetrov@edu.hse.ru>
2024-01-21 23:52:57 +0300

    Выполнил свой вариант (шаблон конвертации массива одного типа в другой)

--- main.cpp
+++ main.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 using namespace std;
 
-template<typename T>
-const T* max(const T array[], size_t n){
-    const T* maximum = &array[0];
-    for (size_t i = 0; i < n; ++i) if (array[i] > *maximum) maximum = &array[i];
-    return maximum;
-}
-
-template<typename T>
-const T* min(const T array[], size_t n){
-    const T* minimum = &array[0];
-    for (size_t i = 0; i < n; ++i) if (array[i] < *minimum) minimum = &array[i];
-    return minimum;
+template<typename InputType, typename OutputType>
+OutputType* converting(const InputType InputArray[], size_t n){
+    OutputType* OutputArray = new OutputType[n];
+    for (size_t i = 0; i < n; ++i){
+        OutputArray[i] = static_cast<OutputType>(InputArray[i]);
+    }
+    return OutputArray;
 }
 
 int main(){
-    size_t N = 5;
-    int *int_array = new int[N]{-3545, 53, -663, 32, 4};
-    double *double_array = new double[N]{-5534445.4445, 4.4, -5346.0, 6663.533, 53.5};
-    unsigned int *unsigned_int_array = new unsigned int[N]{4, 64, 66666, 2, 5};
-    cout << *max(int_array, N) << ' ' << *min(int_array, N) << '\n';
-    cout << *max(double_array, N) << ' ' << *min(double_array, N) << '\n';
-    cout << *max(unsigned_int_array, N) << ' ' << *min(unsigned_int_array, N) <<'\n';
+    size_t n = 5;
+    int *int_array = new int[n]{-3545, 53, -663, 32, 4};
+
+    char* char_array = converting<int, char>(int_array, n);
+    unsigned int* unsigned_int_array = converting<int, unsigned int>(int_array, n);
+
+    for (int i=0; i < n; ++i) cout << int_array[i] << " " << char_array[i] << " " << unsigned_int_array[i] << "\n";
 
     // освобождение памяти
     delete[] int_array;
-    delete[] double_array;
+    delete[] char_array;
     delete[] unsigned_int_array;
 
     return 0;
